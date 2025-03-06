eb6ed1dd313da4de1b09af4200cac581066ea689
Matheus Pozett <maa_souza11@hotmail.com>
2025-03-06 11:22:52 -0300

    cria função de busca por idade

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
+  const animalsOlderThan = data.species
+    .find((specie) => specie.name === animal)
+    .residents.every((resident) => resident.age >= age);
+
+  return animalsOlderThan;
 };
+
+console.log(getAnimalsOlderThan('otters', 7));
 
 module.exports = getAnimalsOlderThan;
