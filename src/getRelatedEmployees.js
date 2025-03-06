f9fafa3dc1fb750adb08d68b3251e3316f49d5f5
Matheus Pozett <maa_souza11@hotmail.com>
2025-03-06 14:38:17 -0300

    req4" q

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,24 @@
 const data = require('../data/zoo_data');
 
+const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
+const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
+const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+
 const isManager = (id) => {
-  // seu código aqui
+  if (id === '9e7d4524-363c-416a-8759-8aa7e50c0992' || id === olaId || id === burlId) {
+    return true;
+  }
+  return false;
 };
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  if (isManager(managerId) === true) {
+    return data.employees.filter((e) => e.managers.includes(managerId))
+      .map((e) => `${e.firstName} ${e.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 };
+
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 module.exports = { isManager, getRelatedEmployees };
