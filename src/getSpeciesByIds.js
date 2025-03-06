43fcd577924bf50b0e50b4f0856e4091abe5ec71
Matheus Pozett <maa_souza11@hotmail.com>
2025-03-06 10:27:59 -0300

    Cria função que filtra animais pelo Id

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  // seu código aqui
+const getSpeciesByIds = (...ids) => {
+  if (!ids) {
+    return [];
+  }
+  const speciesById = data.species.filter((specie) => ids.includes(specie.id));
+
+  return speciesById;
 };
 
+console.log(getSpeciesByIds(
+  '0938aa23-f153-4937-9f88-4858b24d6bce', '78460a91-f4da-4dea-a469-86fd2b8ccc84',
+));
 module.exports = getSpeciesByIds;
